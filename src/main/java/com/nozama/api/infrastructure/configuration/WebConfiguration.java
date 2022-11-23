3c39b2020f2286484abffcfb81549ce9324000f3
thiagomarqs <thiagodsmarques@hotmail.com>
2022-11-22 23:11:12 -0300

    CORS configuration

--- src/main/java/com/nozama/api/infrastructure/configuration/WebConfiguration.java
+++ src/main/java/com/nozama/api/infrastructure/configuration/WebConfiguration.java
@@ -2,25 +2,39 @@ package com.nozama.api.infrastructure.configuration;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        Map<String, MediaType> mediaTypes = Map.of(
-            "json", MediaType.APPLICATION_JSON,
-            "xml", MediaType.APPLICATION_XML
-        );
+  @Value("${cors.allowed-origins:*}")
+  private String corsAllowedOrigins = "";
 
-        configurer
-            .favorParameter(false)
-            .ignoreAcceptHeader(false)
-            .defaultContentType(MediaType.APPLICATION_JSON)
-            .mediaTypes(mediaTypes);
+  @Override
+  public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+    Map<String, MediaType> mediaTypes = Map.of(
+        "json", MediaType.APPLICATION_JSON,
+        "xml", MediaType.APPLICATION_XML);
+
+    configurer
+        .favorParameter(false)
+        .ignoreAcceptHeader(false)
+        .defaultContentType(MediaType.APPLICATION_JSON)
+        .mediaTypes(mediaTypes);
+  }
+
+  @Override
+    public void addCorsMappings(CorsRegistry registry) {    
+      registry
+        .addMapping("/api/**")
+        .allowedOrigins(corsAllowedOrigins)
+        .allowedMethods("*")
+        .allowCredentials(true);
     }
+
 }
